7503a0d877baf98cbd1204f544e67f85e0ac1dea
AJAYKRISHNAN S <sjajaykrishna0808@gmail.com>
2024-08-24 11:58:33 +0530

    mongodb url

--- backend/src/Configurations/config.js
+++ backend/src/Configurations/config.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 module.exports = {
     port: process.env.PORT,
-    DBuri: process.env.DB_URI,
+    DBuri: "mongodb://localhost:27017/TASK_MANAGEMENT",
     secretkey: process.env.SECRET_KEY,
     jwtExpiresForAcessToken: "3d",
     jwtExpiresForRefreshToken: "5d"
